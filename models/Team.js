dd78308750c3b0d1f36e9e7abc2fe2c21fff64cb
vasilignatov <76693214+vasilignatov@users.noreply.github.com>
2023-03-28 15:58:06 +0300

    Added Team mongoose model

---
+++ models/Team.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+
+const teamSchema = new mongoose.Schema({
+    firstName: String,
+    lastName: String,
+    age: Number,
+    jobPosition: String,
+    expirence: String,
+    imageUrl: {
+        type: String,
+
+    },
+    social: {
+        facebook: String,
+        tweeter: String,
+        googleplus: String,
+        youtube: String
+    },
+    description: String
+});
+
+const Team = mongoose.model('Team', teamSchema);
+export default Team;
