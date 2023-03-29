75908f366ec5f5b991ad46cb954b5b1bf7c22d05
vasilignatov <76693214+vasilignatov@users.noreply.github.com>
2023-03-29 23:17:45 +0300

    Added product contoller

---
+++ controllers/product-controller.js
@@ -0,0 +1,15 @@
+import { Router } from 'express';
+import * as productService from '../services/product-service.js';
+
+const router = Router();
+
+router.get('/',(req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+});
+
+
+export default router;
--- middlewares/requestLogгer.js
+++ middlewares/requestLogгer.js
@@ -1,4 +1,4 @@
 export const requestLogger = (req, res, next) => {
-    console.log(req.method);
+    console.log(req.method + ': ' + req.url);
     next();
 }
