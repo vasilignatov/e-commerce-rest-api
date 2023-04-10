4d05cfa97ed8db8be025687bd1ac25313d0ab2d2
vasilignatov <76693214+vasilignatov@users.noreply.github.com>
2023-04-10 22:24:34 +0300

    Added cookie-parser

--- config/express-config.js
+++ config/express-config.js
@@ -1,15 +1,15 @@
 import express from 'express';
 import cors from 'cors';
-import routes from '../routes.js';
+import cookieParser from 'cookie-parser';
 
+import routes from '../routes.js';
 import { requestLogger } from '../middlewares/requestLogгer.js'
 
 export default function expressConfig(server) {
     server.use(express.json());
     server.use(express.urlencoded({ extended: true }));
-
     server.use(cors());
-
     server.use(requestLogger);
+    server.use(cookieParser());
     server.use(routes);
 }
