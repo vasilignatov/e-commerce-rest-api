dd19bc4cb846727b8ac4bf69b9b3df49b5f52c4a
vasilignatov <76693214+vasilignatov@users.noreply.github.com>
2023-04-10 22:25:33 +0300

    Added constants for jwt and bcrypt

---
+++ constants.js
@@ -0,0 +1,3 @@
+export const SALT_ROUNDS = 10;
+export const JWT_SECRET = 'D3ZH^anNL5UfUpW9#^M%jRgTJxnfquhJq7!EAqEg7t$@_N+7?N@&a$gPdmsn5$EM%JeSK&Y$6zL4+YA?';
+export const COOKIE_NAME = 'quiz_auth_token';
