c7c0cff266d361370815fb140e03bcf2b3856e8a
vasilignatov <76693214+vasilignatov@users.noreply.github.com>
2023-04-10 22:23:46 +0300

    Added User model

---
+++ models/User.js
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+import { SALT_ROUNDS } from '../constants.js';
+
+const userSchema = new mongoose.Schema({
+    username: String,
+    email: String,
+    password: String,
+    role: String
+});
+
+userSchema.pre('save', function (next) {
+    bcrypt.hash(this.password, SALT_ROUNDS)
+        .then(hash => {
+            this.password = hash;
+            return next();
+        });
+});
+
+userSchema.method('validatePassword', function (password) {
+    return bcrypt.compare(password, this.password);
+});
+
+const User = mongoose.model('User', userSchema);
+export default User;
