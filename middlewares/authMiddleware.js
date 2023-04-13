5b4c507add038491686e7911184f8d3999e8eca3
vasilignatov <76693214+vasilignatov@users.noreply.github.com>
2023-04-13 22:17:13 +0300

    Edited auth middleware

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,16 +1,16 @@
-import jwt from '../utils/jwtUtils/js';
-import {JWT_SECRET} from '../constants.js';
+import * as jwt from '../utils/jwtUtils.js';
+import { JWT_SECRET } from '../constants.js';
 
 export async function auth(req, res, next) {
-    let token = req.header['x-parse-session-token'];
+    let token = req.headers['x-parse-session-token'];
 
-    if(token) {
+    if (token) {
         try {
-            const decoded = await jwt.verify(token);
+            const decoded = await jwt.verify(token, JWT_SECRET);
             req.user = decoded;
             return next();
         } catch (error) {
-            res.status(401).json({message: 'You are not authorized!'});
+            res.status(401).json({ message: 'You are not authorized!' });
         }
     }
 
@@ -19,5 +19,7 @@ export async function auth(req, res, next) {
 
 // TODO: Route Guards
 // isAuth
+
 // isGuest
+
 // isAdmin
