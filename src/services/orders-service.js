da6a5afb2498424ae497a1b09460cb6bc21ca48a
Vasil <vasilignatow@gmail.com>
2023-10-17 11:41:55 +0300

    removed old files

--- src/services/orders-service.js
+++
@@ -1,7 +0,0 @@
-const Order = require('../models/Order.js');
-
-exports.create = (data) => Order.create(data);
-
-exports.getAll = () => Order.find({});
-
-exports.getOrderById = (id) => Order.findById(id);
--- src/services/team-service.js
+++
@@ -1,5 +0,0 @@
-const Team = require('../models/Team');
-
-exports.getTeam = () => Team.find({});
-
-exports.createTeam = (data) => Team.create(data);
