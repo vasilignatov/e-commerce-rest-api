2254b77090b8cec4808e50c4fcedd298653316cb
Vasil <vasilignatow@gmail.com>
2023-10-17 11:43:28 +0300

    Update whishlist.route.js

--- src/routes/whishlist.route.js
+++ src/routes/whishlist.route.js
@@ -3,5 +3,6 @@ const whishlistController = require('../controllers/whishlist.controller')
 
 router.get('/', whishlistController.getWhishlist);
 router.post('/', whishlistController.addToWhishlist);
+router.delete('/', whishlistController.removeFromWhishlist);
 
 module.exports = router;
