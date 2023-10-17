df70b3a2b582f0c9a6f9f6a22b28caf396549485
Vasil <vasilignatow@gmail.com>
2023-10-17 11:45:41 +0300

    Edited Order model

--- src/models/Order.js
+++ src/models/Order.js
@@ -4,12 +4,19 @@ const orderSchema = new mongoose.Schema({
     firstName: String,
     lastName: String,
     address: String,
-    onDelivery: String,
+    status: {
+        type: String,
+        enum: ['shipped', 'pending', 'recived', 'completed'],
+        default: 'pending'
+    },
     grandTotal: Number,
     userId: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User'
-    }
+    },
+    items: Array
+},{
+    timestamps: true
 });
 
 const Order = mongoose.model('Order', orderSchema);
