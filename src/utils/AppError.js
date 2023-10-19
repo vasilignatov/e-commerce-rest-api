765b5700dc163c117cfcebfd7dc13e41e6439b3e
vasilignatov <76693214+vasilignatov@users.noreply.github.com>
2023-10-19 10:29:03 +0300

    Update AppError.js

--- src/utils/AppError.js
+++ src/utils/AppError.js
@@ -3,7 +3,7 @@ class AppError extends Error {
         super(message);
         this.statusCode = statusCode;
         this.isOperational = isOperational;
-        Error.captureStackTrace(this);
+        Error.captureStackTrace(this, this.constructor);
     }
 }
 
